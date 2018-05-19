1178047263260f1a06f140be0774c998be8e9326
lemonaz5 <gam_wannita@hotmail.com>
2018-05-19 15:43:25 +0700

    mouse interaction

--- main.cpp
+++ main.cpp
@@ -26,6 +26,7 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <math.h>
+#include <vector>
 
 #ifdef __APPLE__
 #include <OpenGL/OpenGL.h>
@@ -38,12 +39,36 @@ using namespace std;
 
 float x = 5;
 float y = 5;
+float anchorX = 0;
+float anchorY = 0;
+float mass = 5;
+float gravity = -10;
+float k = 30; //Spring constant
+float damping = 10; //Damping constant
+float timeStep = 0.07;
+
+
 struct hair{
     float x1;
     float y1;
     float x2;
     float y2;
+    float velocityX;
+    float velocityY;
+    float r;
+    float g;
+    float b;
 };
+
+struct hair hair1 = {.x1=0,.y1=0,.x2=10,.y2=5,.velocityX = 0 ,.velocityY=0,.r = 1.0f,.g = 0.0f,.b = 0.0f};
+struct hair hair2 = {.x1=0.25,.y1=0,.x2=-5,.y2=16,.velocityX = 0 ,.velocityY=0,.r = 0.0f,.g = 1.0f,.b = 0.0f};
+struct hair hair3 = {.x1=-0.1,.y1=0.25,.x2=-5,.y2=15,.velocityX = 0 ,.velocityY=0,.r = 0.0f,.g = 0.0f,.b = 1.0f};
+struct hair hair4 = {.x1=0.1,.y1=0.5,.x2=-1,.y2=1,.velocityX = 0 ,.velocityY=0,.r = 0.0f,.g = 0.0f,.b = 1.0f};
+struct hair hair5 = {.x1=0.24,.y1=0.35,.x2=10,.y2=10,.velocityX = 0 ,.velocityY=0,.r = 0.0f,.g = 0.0f,.b = .0f};
+struct hair hair6 = {.x1=0.,.y1=0.25,.x2=15,.y2=17,.velocityX = 0 ,.velocityY=0,.r = 0.0f,.g = 0.0f,.b = .0f};
+struct hair hair7 = {.x1=0.1,.y1=0.15,.x2=5,.y2=9,.velocityX = 0 ,.velocityY=0,.r = 0.0f,.g = 0.0f,.b = .0f};
+struct hair hair8 = {.x1=0.15,.y1=0.5,.x2=1,.y2=12,.velocityX = 0 ,.velocityY=0,.r = 0.0f,.g = 0.0f,.b = .0f};
+vector<struct hair> hairs;
 
 void handleKeypress(unsigned char key, int x, int y) {
 	switch (key) {
@@ -52,13 +77,77 @@ void handleKeypress(unsigned char key, int x, int y) {
 	}
 }
 
+static BOOL g_bButton1Down = FALSE;
+static int g_yClick = 0;
+static int g_xClick = 0;
+void MouseButton(int button, int state, int x, int y)
+{
+	// Respond to mouse button presses.
+	// If button1 pressed, mark this state so we know in motion function.
+
+	if (button == GLUT_LEFT_BUTTON)
+	{
+		g_bButton1Down = (state == GLUT_DOWN) ? TRUE : FALSE;
+		g_yClick = y;
+		g_xClick = x;
+	}
+}
+
+void MouseMotion(int x, int y)
+{
+	// If button1 pressed, zoom in/out if mouse is moved up/down.
+
+	if (g_bButton1Down)
+	{
+	    if (anchorY>7.8 && g_yClick-y>0) {
+            anchorY=7.8;
+            /*for (int i = 0; i<hairs.size();i++){
+                hairs[i].y1 = (static_cast <float> (rand()) / static_cast <float> (RAND_MAX))+7.5;
+            }*/
+        }else if(anchorY<-7.8 && g_yClick-y<0){
+            anchorY=-7.8;
+            /*for (int i = 0; i<hairs.size();i++){
+                //hairs[i].y1 = hairs[i].y1;
+                hairs[i].y1 = (static_cast <float> (rand()) / static_cast <float> (RAND_MAX))-8.5;
+            }*/
+        }else{
+            anchorY += (g_yClick - y)*0.0005 ;
+            for (int i = 0; i<hairs.size();i++){
+                if((anchorY<7.9) && (anchorY>-7.9)) hairs[i].y1 += (g_yClick - y)*0.0005;
+                else hairs[i].y1 = hairs[i].y1;
+            }
+        }
+
+        if (anchorX>7.8 && x-g_xClick>0){
+            anchorX=7.8;
+            /*for (int i = 0; i<hairs.size();i++){
+                hairs[i].x1 = (static_cast <float> (rand()) / static_cast <float> (RAND_MAX))+7.5;
+            }*/
+        }else if (anchorX<-7.8 && x-g_xClick<0){
+            anchorX=-7.8;
+            /*for (int i = 0; i<hairs.size();i++){
+                hairs[i].x1 = (static_cast <float> (rand()) / static_cast <float> (RAND_MAX))-8.5;
+            }*/
+        }else{
+            anchorX += (x-g_xClick)*0.0005 ;
+            for (int i = 0; i<hairs.size();i++){
+                if((anchorX<8) && (anchorX>-8)) hairs[i].x1 += (x-g_xClick)*0.0005;
+                else hairs[i].x1 = hairs[i].x1;
+            }
+        }
+        //cout << "X" << x << "Y" << y << endl;
+        cout << "xcli" << g_xClick-x << "  ycli" << g_yClick-y<< endl;
+        cout << "hair x " << hairs[0].x1 << " y " << hairs[0].y1 << endl;
+
+		glutPostRedisplay();
+	}
+}
 void initRendering() {
 	glEnable(GL_DEPTH_TEST);
 	/*glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 	glEnable(GL_NORMALIZE);*/
 	glEnable(GL_COLOR_MATERIAL);
-
 }
 
 void handleResize(int w, int h) {
@@ -84,94 +173,92 @@ void drawScene() {
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor);
 	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
 
-    //First Box
-	//glRotatef(-_angle, 1.0f, 1.0f, 0.0f);
-    glLineWidth(1.0);
-    glBegin(GL_LINES);
-    //vertex 1/2 for one line
-    glColor3f( 1.0f, 1.0f, 1.0f);
-    glVertex2f(-0.5f, -0.3f);
-    //vertex 2/2 for one line
-    glVertex2f(0.5f, 0.3f);
+    //First Hair
+    for(int i = 0;i<hairs.size(); i++){
+        glLineWidth(1.0);
+        glBegin(GL_LINES);
+        glColor3f(hairs[i].r,hairs[i].g,hairs[i].b);
+        glVertex2f(hairs[i].x1, hairs[i].y1);
+        glVertex2f(hairs[i].x2, hairs[i].y2);
+        glEnd();
+    }
 
-    glEnd();
+    //Sphere Anchor
+    glPushMatrix();
+    glTranslatef(anchorX,anchorY,0.0f);
+    glColor3f(0.0f, 0.0f, 0.0f);
+    glutSolidSphere(0.7, 15, 15);
+    glPopMatrix();
 
-/*
-	glBegin(GL_TRIANGLES);
-
-	//Tri1
-	glColor3f(1.0f, 0.0f, 0.0f);
-	//glNormal3f(0.0, 1.0f, 0.0f);
-	glVertex3f(0.0f, 2*BOX_SIZE, 0.0f);
-	glVertex3f(-BOX_SIZE, 0.0f, BOX_SIZE);
-	glVertex3f(BOX_SIZE, 0.0f, BOX_SIZE);
-
-    glColor3f(0.0f, 1.0f, 0.0f);
-	//glNormal3f(0.0, 1.0f, 0.0f);
-	glVertex3f(0.0f, 2*BOX_SIZE, 0.0f);
-	glVertex3f(BOX_SIZE, 0.0f, -BOX_SIZE);
-	glVertex3f(BOX_SIZE, 0.0f, BOX_SIZE);
-
-	glColor3f(0.0f, 0.0f, 1.0f);
-	//glNormal3f(0.0, 1.0f, 0.0f);
-	glVertex3f(0.0f, 2*BOX_SIZE, 0.0f);
-	glVertex3f(BOX_SIZE, 0.0f, -BOX_SIZE);
-	glVertex3f(-BOX_SIZE, 0.0f, -BOX_SIZE);
-
-	glColor3f(1.0f, 1.0f, 0.0f);
-	//glNormal3f(0.0, 1.0f, 0.0f);
-	glVertex3f(0.0f, 2*BOX_SIZE, 0.0f);
-	glVertex3f(-BOX_SIZE, 0.0f, +BOX_SIZE);
-	glVertex3f(-BOX_SIZE, 0.0f, -BOX_SIZE);
-
-	glEnd();
-
-	glEnable(GL_TEXTURE_2D);
-	glBindTexture(GL_TEXTURE_2D, _textureId);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    //Background
+    glBegin(GL_QUADS);
 	glColor3f(1.0f, 1.0f, 1.0f);
-	glBegin(GL_QUADS);
-
-	//Down face
-	//glNormal3f(0.0, 0.0f, -1.0f);
-	glTexCoord2f(0.0f, 0.0f);
-	glVertex3f(-BOX_SIZE, 0.0f, -BOX_SIZE);
-	glTexCoord2f(1.0f, 0.0f);
-	glVertex3f(BOX_SIZE, 0.0f, -BOX_SIZE);
-	glTexCoord2f(1.0f, 1.0f);
-	glVertex3f(BOX_SIZE, 0.0f, BOX_SIZE);
-	glTexCoord2f(0.0f, 1.0f);
-	glVertex3f(-BOX_SIZE, 0.0f, BOX_SIZE);
-
+	glVertex2f(-400,400);
+	glVertex2f(-400,-400);
+	glVertex2f(400,-400);
+	glVertex2f(400,400);
 	glEnd();
-*/
-	glDisable(GL_TEXTURE_2D);
 
 	glutSwapBuffers();
 }
 
-//Called every 25 milliseconds
 void update(int value) {
-	/*_angle += 0.8f;
-	if (_angle > 360) {
-		_angle -= 360;
-	}
+    //hairs.push_back(hair1);
+    for(int i=0;i<hairs.size();i++){
+        float springForceY = -k*(hairs[i].y2 - hairs[i].y1);
+        float springForceX = -k*(hairs[i].x2 - hairs[i].x1);
+
+        float dampingForceY = damping * hairs[i].velocityY;
+        float dampingForceX = damping * hairs[i].velocityX;
+
+        float forceY = springForceY + mass * gravity  - dampingForceY;
+        float forceX = springForceX - dampingForceX;
+
+        float accelerationY = forceY/mass;
+        float accelerationX = forceX/mass;
+
+        hairs[i].velocityY += accelerationY*timeStep;
+        hairs[i].velocityX += accelerationX*timeStep;
+
+        hairs[i].x2 += hairs[i].velocityX*timeStep;
+        hairs[i].y2 += hairs[i].velocityY*timeStep;
+
+        /*cout<<"springForceY = "<<springForceY;
+        cout<<"forceY = "<<forceY;
+        cout<<"accelerationY = "<<accelerationY;
+        cout<<"hair1.velocityY = "<<hairs[i].velocityY;
+        cout<<"y2 = "<<hairs[i].y2;
+        cout<<"DDD"<<hairs[i].y2;
+        cout<<"\n";
+        */
+    }
+
 	glutPostRedisplay();
-	glutTimerFunc(25, update, 0);*/
+	glutTimerFunc(25, update, 0);
 }
 
 int main(int argc, char** argv) {
+    hairs.push_back(hair1);
+    hairs.push_back(hair2);
+    hairs.push_back(hair3);
+    hairs.push_back(hair4);
+    hairs.push_back(hair5);
+    hairs.push_back(hair6);
+    hairs.push_back(hair7);
+    hairs.push_back(hair8);
+
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(400, 400);
+	glutInitWindowSize(800, 800);
 
-	glutCreateWindow("Putting It All Together - videotutorialsrock.com");
+	glutCreateWindow("Vorawee");
 	initRendering();
 
 	glutDisplayFunc(drawScene);
 	glutKeyboardFunc(handleKeypress);
 	glutReshapeFunc(handleResize);
+    glutMouseFunc (MouseButton);
+	glutMotionFunc (MouseMotion);
 	glutTimerFunc(25, update, 0);
 	glutMainLoop();
 	return 0;
